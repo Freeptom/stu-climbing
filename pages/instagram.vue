2c84ba736be63973e2527ce8c02305199c71f701
Freeptom <32514405+Freeptom@users.noreply.github.com>
2022-02-17 21:43:34 +0000

    add tokens, insta variables. Get posts and display

---
+++ pages/instagram.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4>Raw data</h4>
+    <textarea v-model="postToString" style="width: 100%; height: 400px">
+    </textarea>
+  </div>
+</template>
+
+<script>
+import { instaDetails } from '~/static/insta'
+export default {
+  async asyncData({ $config: { inToken }, $axios }) {
+    const url = `${instaDetails.RootUrl}/${instaDetails.UserId}/media?access_token=${inToken}&fields=id,caption,media_type,media_url,timestamp`
+    const posts = await $axios.$get(url)
+    const postToString = JSON.stringify(posts, undefined, 4)
+    return { posts, postToString }
+  },
+}
+</script>
---
+++ static/insta.js
@@ -0,0 +1,4 @@
+export const instaDetails = {
+  RootUrl: `https://graph.instagram.com`,
+  UserId: `17841401607721888`,
+}
