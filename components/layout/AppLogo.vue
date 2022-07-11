ca63cb431fc06e7e969f84aea2bf6a826c3ef3be
Freeptom <32514405+Freeptom@users.noreply.github.com>
2022-07-11 13:22:33 +0100

    add missing alt

--- components/layout/AppLogo.vue
+++ components/layout/AppLogo.vue
@@ -1,7 +1,7 @@
 <template>
   <nuxt-link to="/" class="app-logo"
     >Crag Stories
-    <img src="~/assets/images/logo-mark.svg" />
+    <img src="~/assets/images/logo-mark.svg" alt="Crag Stories" />
   </nuxt-link>
 </template>
 
