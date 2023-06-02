9f3700c068121fbab7220471dd74453eb470db16
Freeptom <32514405+Freeptom@users.noreply.github.com>
2023-06-02 10:43:09 +0100

    add whatsapp link test

--- components/landingPage/IntroSection.vue
+++ components/landingPage/IntroSection.vue
@@ -5,9 +5,13 @@
       <template #cta
         ><a :href="contactDetails.email" target="_blank"
           >Book your next adventure &#10141;</a
-        ></template
-      >
+        >
+      </template>
     </app-cta>
+    <a
+      href="https://wa.me/447962225572?text=I'm%20interested%20in%20your%20car%20for%20sale"
+      >Whatsapp</a
+    >
   </app-section>
 </template>
 
