ff53dae02ed958d1fae29a4f53c86e9c63a3ed9e
Freeptom <32514405+Freeptom@users.noreply.github.com>
2023-06-02 11:04:08 +0100

    update whatsapp link text, hide phone icon

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const contactDetails = {
-  whatsapp: `https://wa.me/447962225572?text=I'm%20interested%20in%20your%20car%20for%20sale`,
+  whatsapp: `https://wa.me/447962225572?text=I'm%20interested%20in%20finding%20new%20stories%20with%20you`,
   // phone: 'tel:+447962225572',
   email: 'mailto:stu@cragstories.co.uk',
   instagram: 'https://www.instagram.com/crag_stories/?hl=en',
