2582f8e32112b88994470ee4280ac450885428ff
Freeptom <32514405+Freeptom@users.noreply.github.com>
2023-06-02 10:50:30 +0100

    add whatsapp link to footer

--- components/landingPage/IntroSection.vue
+++ components/landingPage/IntroSection.vue
@@ -8,10 +8,6 @@
         >
       </template>
     </app-cta>
-    <a
-      href="https://wa.me/447962225572?text=I'm%20interested%20in%20your%20car%20for%20sale"
-      >Whatsapp</a
-    >
   </app-section>
 </template>
 
