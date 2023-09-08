48a0171fd27265b62479a319006b9ef12af7d524
Freeptom <32514405+Freeptom@users.noreply.github.com>
2023-09-08 13:35:41 +0100

    add cookies page

--- components/landingPage/AppFooter.vue
+++ components/landingPage/AppFooter.vue
@@ -1,21 +1,19 @@
 <template>
   <footer class="footer">
     <p>&copy; Crag Stories</p>
-    <ul>
-      <li v-for="(value, name, index) in contactDetails" :key="index">
-        <a :href="value" target="_blank">
-          <img
-            :src="require(`~/assets/images/icons/${name}.svg`)"
-            :alt="name"
-          />
-        </a>
-      </li>
-    </ul>
-    <script
-      id="CookieDeclaration"
-      src="https://consent.cookiebot.com/992fd377-f74c-4232-9237-c90a462fe821/cd.js"
-      type="text/javascript"
-    ></script>
+    <div class="footer__links">
+      <ul>
+        <li v-for="(value, name, index) in contactDetails" :key="index">
+          <a :href="value" target="_blank">
+            <img
+              :src="require(`~/assets/images/icons/${name}.svg`)"
+              :alt="name"
+            />
+          </a>
+        </li>
+      </ul>
+      <NuxtLink to="/cookies">Cookies</NuxtLink>
+    </div>
   </footer>
 </template>
 
---
+++ pages/cookies/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper">
+    <app-nav />
+    <div class="h-50">
+      <script
+        id="CookieDeclaration"
+        src="https://consent.cookiebot.com/992fd377-f74c-4232-9237-c90a462fe821/cd.js"
+        type="text/javascript"
+      ></script>
+    </div>
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import AppNav from '~/components/layout/AppNav'
+import AppFooter from '~/components/landingPage/AppFooter'
+
+export default {
+  name: 'Home',
+  components: {
+    AppNav,
+    AppFooter,
+  },
+  layout: 'landing-page',
+  head() {
+    return {
+      title: 'Crag Stories - Cookies',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Take your stories to new heights with Crag Stories. Explore thrilling adventures guided by outdoor instructor, Stuart Serrechia. From beginners to seasoned adventures, discover tailored experiences that blend skill-building with breathtaking views. Unleash your inner adventurer today!',
+        },
+      ],
+    }
+  },
+}
+</script>
